1181d46cda9e8c050a5a6d1b19857f25f25a78b1
Lucas Coelho <lucascoelho@airia.com>
2024-12-17 08:15:11 -0300

    Initial Commit

---
+++ Weather Tracker/Weather_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TrackerApp.swift
+//  Weather Tracker
+//
+//  Created by Lucas Coelho on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
